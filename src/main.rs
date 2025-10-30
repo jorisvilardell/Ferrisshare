6cfc381b1e0ed5ba5e5019aa720c84be3940c6f3
ZUHOWKS <jojo.vilard38@gmail.com>
2025-10-30 13:40:34 +0100

    feat(main): refactor main function to initialize services with storage repository

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,43 @@ use std::sync::Arc;
 
 use tokio::{net::TcpStream, sync::mpsc};
 
-use ferrisshare::core::domain::{
-    command::services::CommandServiceImpl,
-    network::{ports::NetworkService as _, services::NetworkServiceImpl},
+use ferrisshare::{
+    core::domain::{
+        command::services::CommandServiceImpl,
+        network::{ports::NetworkService as _, services::NetworkServiceImpl},
+    },
+    infra::repositories::fs::fs_storage_repository::FSStorageRepository,
 };
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
     let (tx, rx) = mpsc::channel::<TcpStream>(1);
 
-    let command_service = CommandServiceImpl::new();
-    let network_service = Arc::new(NetworkServiceImpl::new(command_service));
+    let storage_repo = FSStorageRepository::new("./public/".into());
 
-    let handler_service = Arc::clone(&network_service);
+    let command_service = CommandServiceImpl::new(storage_repo);
+    let network_service = NetworkServiceImpl::new(command_service);
+
+    let ferrisshare_state = Arc::new(
+        ferrisshare::application::ferrisshare_state::FerrisShareState::new(
+            network_service.command_service.clone(),
+            network_service.clone(),
+        ),
+    );
+
+    let ferrisshare_state_clone = ferrisshare_state.clone();
+
     tokio::spawn(async move {
-        if let Err(e) = handler_service.handler(rx).await {
+        if let Err(e) = ferrisshare_state_clone.network_service.handler(rx).await {
             eprintln!("Handler error: {}", e);
         }
     });
 
-    if let Err(e) = network_service.listener("127.0.0.1:9000", tx).await {
+    if let Err(e) = ferrisshare_state
+        .network_service
+        .listener("127.0.0.1:9000", tx)
+        .await
+    {
         eprintln!("Listener error: {:?}", e);
     }
     Ok(())
